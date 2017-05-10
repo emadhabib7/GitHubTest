3857bac1710bc116ec13747014a3e16c670c8c59
Amir <amir@Amirs-Mac.local>
2017-05-10 19:12:13 +0200

    Initial Commit

---
+++ GitHubTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitHubTest
+//
+//  Created by Amir on 9/2/1733 ERA1.
+//  Copyright © 1733 ERA1 Emad. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Amir on 9/2/1733 ERA1.
+//  Copyright © 1733 ERA1 Emad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
